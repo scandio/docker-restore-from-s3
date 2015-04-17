f6488c3646bebe470bad4cf3911fd0cd9f63eb45
goellnerit <philipp@goellner.it>
2015-04-17 14:52:24 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,14 +4,8 @@ ACCESS_KEY=${ACCESS_KEY:?"ACCESS_KEY env variable is required"}
 SECRET_KEY=${SECRET_KEY:?"SECRET_KEY env variable is required"}
 S3_PATH=${S3_PATH:?"S3_PATH env variable is required"}
 DATA_PATH=${DATA_PATH:-/data/}
-CRON_SCHEDULE=${CRON_SCHEDULE:-0 1 * * *}
 
 echo "access_key=$ACCESS_KEY" >> /root/.s3cfg
 echo "secret_key=$SECRET_KEY" >> /root/.s3cfg
 
-if [[ "$1" == 'no-cron' ]]; then
-    exec /usr/bin/s3cmd sync "$DATA_PATH" "$S3_PATH"
-else
-    echo "$CRON_SCHEDULE /usr/bin/s3cmd sync \"$DATA_PATH\" \"$S3_PATH\"" | crontab -
-    exec cron -f
-fi
+exec /usr/bin/s3cmd sync "$S3_PATH" "$DATA_PATH"
